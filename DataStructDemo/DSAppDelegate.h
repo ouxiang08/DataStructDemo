9e7901c1dcbb696a91612de0a3b052f3fc2f9114
JINGJING.JIA <jingjing.jia@sensu-sh.com>
2014-11-11 17:48:37 +0800

    Initial Commit

---
+++ DataStructDemo/DSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSAppDelegate.h
+//  DataStructDemo
+//
+//  Created by apple on 14/11/11.
+//  Copyright (c) 2014年 jingjing.jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DataStructDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DataStructDemo
+//
+//  Created by apple on 14/11/11.
+//  Copyright (c) 2014年 jingjing.jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSAppDelegate class]));
+    }
+}
